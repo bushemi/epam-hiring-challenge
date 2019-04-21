69d745fe9fcb907df7c456d78c8fb5a4c2d06599
Igor <bushemi2001@gmail.com>
2019-04-21 18:51:34 +0300

    was added HashCalculator

---
+++ src/main/java/brutForcer/HashCalculator.java
@@ -0,0 +1,13 @@
+package brutForcer;
+
+import java.nio.charset.StandardCharsets;
+import com.google.common.hash.Hasher;
+import com.google.common.hash.Hashing;
+
+public class HashCalculator {
+    public String hash(String toHash) {
+        Hasher hasher = Hashing.md5().newHasher();
+        hasher.putString(toHash, StandardCharsets.UTF_8);
+        return hasher.hash().toString();
+    }
+}
