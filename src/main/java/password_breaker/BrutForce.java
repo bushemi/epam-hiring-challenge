b93b474b065de9f1b2a13f8a3f2a2757511b537d
Igor <bushemi2001@gmail.com>
2019-04-21 20:06:42 +0300

    was added BrutForce class for a searching the password in multiple threads.

---
+++ src/main/java/password_breaker/BrutForce.java
@@ -0,0 +1,58 @@
+package password_breaker;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+class BrutForce {
+    private static HashCalculator calculator = new HashCalculator();
+    private static final String TARGET = "69c459dd76c6198f72f0c20ddd3c9447"; //zebra
+    private static final String TARGET_ADVANCED = "4fd0101ea3d0f5abbe296ef97f47afec";
+    private ThreadPoolExecutor executor;
+
+    private String unHashedTarget = new String();
+    private String unHashedTargetAdvanced = new String();
+
+    BrutForce(int numberOfThreads) {
+        executor = (ThreadPoolExecutor) Executors.newFixedThreadPool(numberOfThreads);
+    }
+
+    void findPasswordRegular() {
+        Password password = new Password();
+        while (true) {
+            executor.submit(() -> {
+                String text = password.getPassword();
+                if (checkForMatch(text)) unHashedTarget = text;
+                return null;
+            });
+
+            if (!unHashedTarget.isEmpty()) break;
+        }
+        System.out.println("unHashedTarget = " + unHashedTarget);
+        executor.shutdown();
+    }
+
+    void findPasswordAdvancedLevel() {
+        Password password = new Password();
+        while (true) {
+            executor.submit(() -> {
+                String text = password.getPassword();
+                if (checkForMatchAdvanced(text)) unHashedTargetAdvanced = text;
+                return null;
+            });
+
+            if (!unHashedTargetAdvanced.isEmpty()) break;
+        }
+        System.out.println("unHashedTargetAdvanced = " + unHashedTargetAdvanced);
+        executor.shutdown();
+    }
+
+    private static boolean checkForMatch(String text) {
+        String newHash = calculator.hash(text);
+        return newHash.equals(TARGET);
+    }
+
+    private static boolean checkForMatchAdvanced(String text) {
+        String newHash = calculator.hash(text);
+        return newHash.equals(TARGET_ADVANCED);
+    }
+}
